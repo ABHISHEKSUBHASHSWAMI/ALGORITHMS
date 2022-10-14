f5e06672d4a356f1308ec7c1ede04c5c97fe4a9e
S ABHISHEK <79701277+ABHISHEKSUBHASHSWAMI@users.noreply.github.com>
2022-10-14 21:51:52 +0530

    Update optimalmerge.py

--- optimalmerge.py
+++ optimalmerge.py
@@ -2,19 +2,24 @@
 
 #program for finding cost for optimal merge pattern
 
+#function to calculate optimal cost
 def optimized_cost(weights):
 
     length=len(weights)
+    #list for path/parent node
     temp=[]
+    
     for i in range(length):
         weights=sorted(weights)
         print("\nSizes\n",weights)
         if len(weights)>1:
+            #merge minimum weight pair and add to both list and finally pop the pair out.
             temp.append(weights[0]+weights[1])
             weights.append(weights[0]+weights[1])
             for j in range(2):
                 weights.pop(0)
     print("\ncost path",temp)
+    #return cost
     return sum(temp)
 
 if __name__ == "__main__":
